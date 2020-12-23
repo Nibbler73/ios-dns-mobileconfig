34f057db71c42cd76ffde1aec8333072d81bef28
Hrvoje Husic <hhusic@gmail.com>
2020-12-23 23:00:46 +0100

    add comment

--- mobileconfig.sh
+++ mobileconfig.sh
@@ -5,6 +5,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd $DIR
 cd unsigned
 
+# loop all mobileconfig-files from unsigned folder
 for i in *.mobileconfig
  do
   /usr/bin/openssl smime -sign -signer path/to/cert.crt -inkey path/to/key.key -certfile path/to/issuer.crt -nodetach -outform der -in $i -out ../signed/$i
